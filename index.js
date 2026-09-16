47b15f1a17b02db68ec47aab7540a12a22ac6cf8
manisandeep14 <manisandeepms32m16@gmail.com>
2026-09-16 20:12:25 +0530

    Implement addTodo function with event listeners for button click and Enter key

--- index.js
+++ index.js
@@ -10,3 +10,57 @@ const remainingTasks = document.querySelector("#remainingTasks");
 let totalCount = 0;
 let completedCount = 0;
 let remainingCount = 0;
+
+
+function addTodo(){
+  if(input.value != ""){
+    let ele = document.createElement("li");
+    ele.classList.add("li-st");
+    list.append(ele);
+    totalCount+=1;
+    totalTasks.textContent = totalCount;
+    remainingCount = totalCount-completedCount;
+    remainingTasks.textContent = remainingCount;
+
+    let div2 = document.createElement("div");
+    div2.classList.add("task-left");
+    ele.append(div2);
+
+    let input0 = document.createElement("input");
+    input0.setAttribute("type", "checkbox");
+    div2.append(input0);
+
+    let spaning = document.createElement("span");
+    spaning.textContent = input.value;
+    div2.append(spaning);
+
+    let div1 = document.createElement("div");
+    div1.classList.add("actions");
+    ele.append(div1);
+ 
+    let btn1 = document.createElement("button");
+    let btn2 = document.createElement("button");
+
+    btn1.textContent = "Delete";
+    btn2.textContent = "Edit";
+ 
+ 
+    div1.append(btn1);
+    div1.append(btn2);
+
+    input.value = "";
+ 
+    saveTasks();
+  }
+}
+
+//on click add to do
+btn.addEventListener('click', addTodo);
+
+//on enter add to do
+input.addEventListener('keydown', (e)=>{
+  if(e.key === 'Enter'){
+    addTodo();
+  }
+});
+
